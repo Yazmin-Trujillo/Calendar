4843075f90a2d8598aa86187addf816d2f3301f9
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-08-16 16:16:04 -0500

    talvez

---
+++ src/Extra.tsx
@@ -0,0 +1,51 @@
+// export interface Week {
+//     sun: string
+//     mon: string
+//     tue: string
+//     wed: string
+//     thu: string
+//     fri: string
+//     sat: string
+// }
+
+import { ImportsNotUsedAsValues } from "typescript"
+
+// export interface Month{
+//     January: string
+//     February: string
+//     March: string
+//     April: string
+//     May: string
+//     June: string
+//     July: string
+//     August: string
+//     September: string
+//     October: string
+//     November: string
+//     December: string
+// }
+
+export enum Week {
+    Sun,
+    Mon,
+    Tue,
+    wed,
+    Thu,
+    Fri,
+    Sat
+}
+
+export enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
