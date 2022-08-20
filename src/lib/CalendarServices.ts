d88adbb85b05dbfa4a0653c914846e8d6c912eb7
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-08-19 22:03:09 -0500

    funcion para agregar los dias del mes siguiente

--- src/lib/CalendarServices.ts
+++ src/lib/CalendarServices.ts
@@ -4,45 +4,23 @@ export const calendarService = {
 
     getCalendarDays: (year: number, month: number) => {
         const dayOneIndex = new Date(year, month, 1).getDay()
-
         const calendarDays: CalendarDay[] = [];
+
         addPreviousMonth(year, month, dayOneIndex, calendarDays);
         addCurrentMonth(year, month, calendarDays, dayOneIndex);
+        addNextMonth(year, month, calendarDays);
 
         return calendarDays;
     }
 }
 
-function addCurrentMonth(year: number, month: number, calendarGrid: CalendarDay[], dayOneIndex: number) {
-    const daysInTheMonth = new Date(year, month + 1, 0).getDate();
-    const today = new Date();
-
-    for (let i = 0; i < daysInTheMonth; i++) {
-        // if()
-        calendarGrid.push({
-            year: year,
-            month: month,
-            day: (i + 1),
-            currentDay: today.getFullYear() === year && today.getMonth() === month && today.getDate() === dayOneIndex + i,
-            otherMonth: false,
-        });
-    }
-}
-
-function addPreviousMonth(year: number, month: number, dayOneIndex: number, calendarGrid: CalendarDay[]) {
+function addPreviousMonth(year: number, month: number, dayOneIndex: number, calendarDays: CalendarDay[]) {
     const previousMonth = new Date(year, month, 0);
-    console.log('previousMonth', previousMonth);
-    console.log('previousMonthDays', new Date(year, month, -1));
-
     const startOfPreviousCells = new Date(year, month, -dayOneIndex).getDate();
-    console.log('starOfPreviouscell', startOfPreviousCells);
+    const daysInPreviousMonth = previousMonth.getDate();
 
-    const daysInPreviosuMonth = previousMonth.getDate();
-
-    console.log('mesAnterior', previousMonth.getFullYear());
-
-    for (let i = startOfPreviousCells; i < daysInPreviosuMonth; i++) {
-        calendarGrid.push({
+    for (let i = startOfPreviousCells; i < daysInPreviousMonth; i++) {
+        calendarDays.push({
             year: previousMonth.getFullYear(),
             month: previousMonth.getMonth(),
             day: i + 1,
@@ -52,3 +30,33 @@ function addPreviousMonth(year: number, month: number, dayOneIndex: number, cale
     }
 }
 
+function addCurrentMonth(year: number, month: number, calendarDays: CalendarDay[], dayOneIndex: number) {
+    const daysInTheMonth = new Date(year, month + 1, 0).getDate();
+    const today = new Date();
+
+    for (let i = 0; i < daysInTheMonth; i++) {
+        calendarDays.push({
+            year: year,
+            month: month,
+            day: (i + 1),
+            otherMonth: false,
+            currentDay: today.getFullYear() === year && today.getMonth() === month && today.getDate() === dayOneIndex + i,
+        });
+    }
+}
+
+function addNextMonth(year: number, month: number, calendarDays: CalendarDay[]) {
+    const nextMonth = new Date(year, month + 2, 0);
+    const lastDayIndex = new Date(year, month + 1, 0).getDay();
+    const daysInNextMonth = 6 - lastDayIndex;
+
+    for (let i = 0; i < daysInNextMonth; i++) {
+        calendarDays.push({
+            year: nextMonth.getFullYear(),
+            month: nextMonth.getMonth(),
+            day: i + 1,
+            otherMonth: true,
+            currentDay: false,
+        });
+    }
+}
