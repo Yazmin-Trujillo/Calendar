a5172857d3815d0f30939d1724b88441f46661c4
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-08-19 21:12:33 -0500

    cambios para agregar los numeros del mes anterior, refactor

--- src/lib/CalendarServices.ts
+++ src/lib/CalendarServices.ts
@@ -3,32 +3,52 @@ import { CalendarDay } from "./Models";
 export const calendarService = {
 
     getCalendarDays: (year: number, month: number) => {
-        const today = new Date()
-        const dayOne = new Date(year, month, 1).getDay()
-        const daysInTheMonth = new Date(year, month+1, 0).getDate()
-        const calendarGrid: CalendarDay[] = new Array(35);
+        const dayOneIndex = new Date(year, month, 1).getDay()
 
-        for (let i = 0; i < calendarGrid.length; i++) {
-            calendarGrid[i] = {
-                year: 0,
-                month: 0,
-                day: 0,
-                otherMonth: true,
-                currentDay: false,
-            };
-        }
+        const calendarDays: CalendarDay[] = [];
+        addPreviousMonth(year, month, dayOneIndex, calendarDays);
+        addCurrentMonth(year, month, calendarDays, dayOneIndex);
 
-        for (let i = 0; i < daysInTheMonth; i++) {
-            // if()
-            calendarGrid[i + dayOne] = {
-                year: year,
-                month: month,
-                day: (i + 1),
-                currentDay: today.getFullYear() === year && today.getMonth() === month && today.getDate() === dayOne + i,
-                otherMonth: false,
-            };
-        }
-        return calendarGrid;
+        return calendarDays;
     }
-
 }
+
+function addCurrentMonth(year: number, month: number, calendarGrid: CalendarDay[], dayOneIndex: number) {
+    const daysInTheMonth = new Date(year, month + 1, 0).getDate();
+    const today = new Date();
+
+    for (let i = 0; i < daysInTheMonth; i++) {
+        // if()
+        calendarGrid.push({
+            year: year,
+            month: month,
+            day: (i + 1),
+            currentDay: today.getFullYear() === year && today.getMonth() === month && today.getDate() === dayOneIndex + i,
+            otherMonth: false,
+        });
+    }
+}
+
+function addPreviousMonth(year: number, month: number, dayOneIndex: number, calendarGrid: CalendarDay[]) {
+    const previousMonth = new Date(year, month, 0);
+    console.log('previousMonth', previousMonth);
+    console.log('previousMonthDays', new Date(year, month, -1));
+
+    const startOfPreviousCells = new Date(year, month, -dayOneIndex).getDate();
+    console.log('starOfPreviouscell', startOfPreviousCells);
+
+    const daysInPreviosuMonth = previousMonth.getDate();
+
+    console.log('mesAnterior', previousMonth.getFullYear());
+
+    for (let i = startOfPreviousCells; i < daysInPreviosuMonth; i++) {
+        calendarGrid.push({
+            year: previousMonth.getFullYear(),
+            month: previousMonth.getMonth(),
+            day: i + 1,
+            otherMonth: true,
+            currentDay: false,
+        });
+    }
+}
+
